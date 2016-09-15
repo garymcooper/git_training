3bd775e15250df1fa339ffe8d4cdc660b5429797
Milne <scott.milne@mpac.ca>
2016-09-15 11:50:48 -0400

    add the summing

--- mpac.r
+++ mpac.r
@@ -1,15 +1,16 @@
+# Read in the (fake) 2020.csv file
+# Read in a text file and make sure that the first column is character
+# http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
+column_classes=c("character","numeric","character")
+data<-read.csv("validate-vars.txt", colClasses=column_classes)
+
+
 # frequency counts
 # compare means
 # cross tabs
 # validate variables
+
 # summing by ID
-
-
-
-# Read in a text file and make sure that the first column is character
-
-# http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
-
-column_classes=c("character","numeric","character")
-
-data<-read.csv("validate-vars.txt", colClasses=column_classes)
+# sum the "TotalCost" column for each "ID" column
+# http://stackoverflow.com/questions/10180132/consolidate-duplicate-rows
+aggdata<-aggregate(data["TotalCost"],by=data["ID"],sum)
