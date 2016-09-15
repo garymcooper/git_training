f2d770fe25df2a8e2b645a62cb021262e070e347
Gary Cooper <COOPERGA@mpac.ca>
2016-09-15 09:49:33 -0400

    add in reading a file called validate-vars.txt with column classes

--- mpac.r
+++ mpac.r
@@ -3,3 +3,13 @@
 # cross tabs
 # validate variables
 # summing by ID
+
+
+
+# Read in a text file and make sure that the first column is character
+
+# http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
+
+column_classes=c("character","numeric","character")
+
+data<-read.csv("validate-vars.txt", colClasses=column_classes)
